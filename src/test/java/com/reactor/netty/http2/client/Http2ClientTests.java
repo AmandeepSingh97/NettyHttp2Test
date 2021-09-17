a7e611b680c17d76e1e1c1d9f6c3e46ad15cf95e
asingh21 <asingh21@paypal.com>
2021-09-17 16:45:17 +0530

    Adding tests to verify multiple connections

--- src/test/java/com/reactor/netty/http2/client/Http2ClientTests.java
+++ src/test/java/com/reactor/netty/http2/client/Http2ClientTests.java
@@ -1,17 +1,28 @@
 package com.reactor.netty.http2.client;
 
+import io.netty.handler.logging.LogLevel;
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import io.netty.handler.ssl.util.SelfSignedCertificate;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 import reactor.netty.DisposableServer;
 import reactor.netty.http.Http2SslContextSpec;
 import reactor.netty.http.HttpProtocol;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.http.server.HttpServer;
+import reactor.netty.transport.logging.AdvancedByteBufFormat;
 import reactor.test.StepVerifier;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.security.cert.CertificateException;
+import java.time.Duration;
+import java.util.List;
+import java.util.Scanner;
 
 public class Http2ClientTests {
 
@@ -29,7 +40,6 @@ public class Http2ClientTests {
                     .protocol(HttpProtocol.H2)
                     .secure(sslContextSpec -> sslContextSpec.sslContext(serverCtx))
                     .port(0)
-                    //.http2Settings(setting -> setting.maxConcurrentStreams(100))
                     .handle((req, res) -> res.sendString(Mono.just(RESPONSE_STR)))
                     .wiretap(true).bindNow();
             System.out.println("Reactor Netty started on " + server.port());
@@ -39,8 +49,16 @@ public class Http2ClientTests {
         }
     }
 
-    @Test
-    public void testHttp2Client() {
+    @Test(dataProvider = "parallelTests")
+    public void testHttp2ClientParallelCalls(int n) throws FileNotFoundException {
+        emptyFile("log-ConnectionPool.txt");
+        final HttpClient client = getHttp2Client();
+        performNParallelCalls(client, n);
+        int connections = getNumEvents("log-ConnectionPool.txt", "CONNECT:");
+        System.out.println("Connections observed in doing " + n +" calls is: " + connections);
+    }
+
+    private HttpClient getHttp2Client() {
         Http2SslContextSpec clientCtx =
                 Http2SslContextSpec.forClient()
                         .configure(builder -> builder.trustManager(InsecureTrustManagerFactory.INSTANCE));
@@ -48,14 +66,65 @@ public class Http2ClientTests {
         final HttpClient client =  HttpClient.create()
                 .secure(sslContextSpec -> sslContextSpec.sslContext(clientCtx))
                 .protocol(HttpProtocol.H2)
-                .wiretap(true);
+                .wiretap("http2PoolTest", LogLevel.INFO, AdvancedByteBufFormat.HEX_DUMP);;
+        return client;
+    }
 
-        StepVerifier.create(client
+    private List<String> performNParallelCalls(HttpClient httpClient, int n) {
+        return Flux.range(0, n)
+                .parallel()
+                .runOn(Schedulers.parallel())
+                .flatMap(item -> getServerHello(httpClient))
+                .sequential()
+                .collectList()
+                .block();
+    }
+
+    private List<String> performNSequentialCalls(HttpClient httpClient, int n, long delay) {
+        return Flux.range(0, n)
+                .delayElements(Duration.ofMillis(delay))
+                .flatMap(item -> getServerHello(httpClient))
+                .collectList()
+                .block();
+    }
+
+    private Mono<String> getServerHello(HttpClient httpClient) {
+        return httpClient
                 .get()
                 .uri("https://localhost:" + http2Server.port())
                 .responseContent()
                 .aggregate()
-                .asString()
-        ).expectNext(RESPONSE_STR).verifyComplete();
+                .asString();
+    }
+
+    private int getNumEvents(String path, String filter) throws FileNotFoundException {
+        File logs = new File(path);
+        Scanner st = new Scanner(logs);
+        int connections = 0;
+        while (st.hasNextLine()) {
+            String line = st.nextLine();
+            if (line.contains(filter)) {
+                connections++;
+            }
+        }
+        return connections;
+    }
+
+    private void emptyFile(String filePath) throws FileNotFoundException {
+        File file = new File(filePath);
+        PrintWriter writer = new PrintWriter(file);
+        writer.print("");
+        writer.close();
+    }
+
+    @DataProvider(name = "parallelTests")
+    public Object[][] parallelTests() {
+        return new Object[][] {
+                { 5 },
+                { 10 },
+                { 20 },
+                { 50 },
+                { 200 }
+        };
     }
 }
